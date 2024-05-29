2556a3895aeb5d9e55e4601869c7667e9d3f4b57
Noorain Panjwani <noorain.panjwani@gmail.com>
2024-05-28 22:44:55 -0700

    Okay fixed some sidebar issue
    Signed-off-by: Noorain Panjwani <noorain.panjwani@gmail.com>

--- frontend/src/routes/tasks/today.tsx
+++ frontend/src/routes/tasks/today.tsx
@@ -68,6 +68,17 @@ export default function Today() {
       effort: 2,
       dueDate: '2024-05-08T00:10:01-07:00',
     },
+    {
+      id: 7,
+      title:
+        'A story from Ahmed Besbes on Medium Read this story from Ahmed Besbes on Medium: Improve RAG Pipelines With These 3 Indexing Methods',
+      description:
+        'This is a story from Ahmed Besbes on Medium. Read this story from Ahmed Besbes on Medium: Improve RAG Pipelines With These 3 Indexing Methods',
+      completed: false,
+      priority: 2,
+      effort: 2,
+      dueDate: '2024-05-08T00:10:01-07:00',
+    },
   ];
 
   const effortList = effortArray
@@ -81,42 +92,44 @@ export default function Today() {
   return (
     <>
       <div>
-        <nav className="hidden sm:flex" aria-label="Breadcrumb">
-          <ol role="list" className="flex items-center space-x-4">
-            <li>
-              <div className="flex">
-                <Link
-                  href="/tasks/today"
-                  className="text-xs font-medium text-gray-500 hover:text-gray-700"
-                >
-                  Tasks
-                </Link>
-              </div>
-            </li>
-            <li>
-              <div className="flex items-center">
-                <ChevronRightIcon
-                  className="h-5 w-5 flex-shrink-0 text-gray-400"
-                  aria-hidden="true"
-                />
-                <Link
-                  href="/tasks/today"
-                  className="ml-4 text-xs font-medium text-gray-500 hover:text-gray-700"
-                >
-                  Today
-                </Link>
-              </div>
-            </li>
-          </ol>
-        </nav>
-        <div className="sm:mt-2 md:flex md:items-center md:justify-between">
-          <div className="min-w-0 flex-1">
-            <Heading>Day at a Glance</Heading>
+        <div>
+          <nav className="hidden sm:flex" aria-label="Breadcrumb">
+            <ol role="list" className="flex items-center space-x-4">
+              <li>
+                <div className="flex">
+                  <Link
+                    href="/tasks/today"
+                    className="text-xs font-medium text-gray-500 hover:text-gray-700"
+                  >
+                    Tasks
+                  </Link>
+                </div>
+              </li>
+              <li>
+                <div className="flex items-center">
+                  <ChevronRightIcon
+                    className="h-5 w-5 flex-shrink-0 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  <Link
+                    href="/tasks/today"
+                    className="ml-4 text-xs font-medium text-gray-500 hover:text-gray-700"
+                  >
+                    Today
+                  </Link>
+                </div>
+              </li>
+            </ol>
+          </nav>
+          <div className="sm:mt-2 md:flex md:items-center md:justify-between">
+            <div className="min-w-0 flex-1">
+              <Heading>Day at a Glance</Heading>
+            </div>
           </div>
         </div>
+        <div className="h-4"></div>
+        <FocusedItem title="K8s Daily Stand Up" type="meeting" />
       </div>
-      <div className="h-4"></div>
-      <FocusedItem title="K8s Daily Stand Up" type="meeting" />
       <div className="h-4"></div>
       {effortList.map(({ effort, tasks }) => (
         <div key={effort}>
